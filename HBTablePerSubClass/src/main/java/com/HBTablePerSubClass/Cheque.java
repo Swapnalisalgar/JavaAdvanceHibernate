3b38cf5af8b9fb18de758ec553874e3108a7810a
Swapnalisalgar <129720475+Swapnalisalgar@users.noreply.github.com>
2023-04-04 17:25:29 +0530

    HBTablePerSubClass
    HBTablePerSubClass

---
+++ HBTablePerSubClass/src/main/java/com/HBTablePerSubClass/Cheque.java
@@ -0,0 +1,30 @@
+package com.HBTablePerSubClass;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "chequeps")
+@AttributeOverrides({
+	@AttributeOverride(name = "paymentId", column = @Column(name = "id")),
+	@AttributeOverride(name = "amount", column = @Column(name = "amount"))
+})
+public class Cheque extends Payment{
+	
+	@Column(name = "ChequeType")
+	private String ChequeType;
+
+	public String getChequeType() {
+		return ChequeType;
+	}
+
+	public void setChequeType(String chequeType) {
+		ChequeType = chequeType;
+	}
+
+		
+}
---
+++ HBTablePerSubClass/src/main/java/com/HBTablePerSubClass/Payment.java
@@ -0,0 +1,42 @@
+package com.HBTablePerSubClass;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "paymentps")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Payment{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private int paymentId;
+	
+	@Column(name = "amount")
+	private double amount;
+	
+	
+	public int getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}	 
+	
+}
